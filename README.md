8827bcc8db8ebf0af73611d330feb583cb2a8bac
anvbis <stephen.t.radley+anvbis@protonmail.com>
2023-03-17 16:18:00 +1100

    added ability to download debug symbols

--- README.md
+++ README.md
@@ -7,28 +7,30 @@ $ git clone https://github.com/anvbis/libsearch
 $ cd libsearch && pip install -r requirements.txt
 ```
 
+
 ## Usage
 
 ```
-usage: libsearch.py [-h] {list,download} ...
+usage: libsearch.py [-h] {list,get-lib,get-dbg} ...
 
 positional arguments:
-  {list,download}
+  {list,get-lib,get-dbg}
 
 options:
-  -h, --help       show this help message and exit
+  -h, --help            show this help message and exit
 ```
 
 ```
-usage: libsearch.py list [-h] [-a ARCH]
+usage: libsearch.py list [-h] [-a ARCH] [-d]
 
 options:
   -h, --help            show this help message and exit
   -a ARCH, --arch ARCH  filter by architecture
+  -d, --debug           list debug symbols
 ```
 
 ```
-usage: libsearch.py download [-h] -t TARGET [-o OUT]
+usage: libsearch.py get-lib [-h] -t TARGET [-o OUT]
 
 options:
   -h, --help            show this help message and exit
@@ -37,7 +39,18 @@ options:
   -o OUT, --out OUT     output file name
 ```
 
-## Example
+```
+usage: libsearch.py get-dbg [-h] -t TARGET [-o OUT]
+
+options:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        target libc debug symbols
+  -o OUT, --out OUT     output directory name
+```
+
+
+## Retrieving a Shared Library
 
 ```
 $ ./libsearch.py list -a amd64
@@ -53,10 +66,41 @@ libc6_2.35-0ubuntu3.1_amd64
 libc6_2.35-0ubuntu3_amd64
 libc6_2.36-0ubuntu4_amd64
 libc6_2.37-0ubuntu1_amd64
+libc6_2.37-0ubuntu2_amd64
 ```
 
 ```
-$ ./libsearch.py download -t libc6_2.27-3ubuntu1.6_amd64
+$ ./libsearch.py get-lib -t libc6_2.31-0ubuntu9.9_amd64
+100%|███████████████████████████████████████████████████████████| 2.60M/2.60M [00:03<00:00, 872kB/s]
 $ ls *.so
-libc6_2.27-3ubuntu1.6_amd64.so
+libc6_2.31-0ubuntu9.9_amd64.so
+```
+
+
+## Retrieving Debug Symbols
+
+```
+$ ./libsearch.py list -a amd64 --debug
+libc6-dbg_2.23-0ubuntu3_amd64
+libc6-dbg_2.23-0ubuntu11.3_amd64
+libc6-dbg_2.27-3ubuntu1.5_amd64
+libc6-dbg_2.27-3ubuntu1.6_amd64
+libc6-dbg_2.27-3ubuntu1_amd64
+libc6-dbg_2.31-0ubuntu9.7_amd64
+libc6-dbg_2.31-0ubuntu9.9_amd64
+libc6-dbg_2.31-0ubuntu9_amd64
+libc6-dbg_2.35-0ubuntu3.1_amd64
+libc6-dbg_2.35-0ubuntu3_amd64
+libc6-dbg_2.36-0ubuntu4_amd64
+libc6-dbg_2.37-0ubuntu1_amd64
+libc6-dbg_2.37-0ubuntu2_amd64
+```
+
+```
+./libsearch.py get-dbg -t libc6-dbg_2.31-0ubuntu9.9_amd64
+100%|██████████████████████████████████████████████████████████| 19.1M/19.1M [00:10<00:00, 1.98MB/s]
+$ stat libc6-dbg_*stat libc6-dbg_*
+  File: libc6-dbg_2.31-0ubuntu9.9_amd64
+  Size: 4096      	Blocks: 8          IO Block: 4096   directory
+  ...
 ```
--- libsearch.py
+++ libsearch.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
-import bs4
+from bs4 import BeautifulSoup
 import os
 import requests
+import shutil
 import tarfile
 import tempfile
 import unix_ar
-import urllib.request
+from tqdm.auto import tqdm
 
 url = 'http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/'
 
 
 def _search_libc(arch='all'):
     r = requests.get(url)
-    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-    is_libc = lambda x: x[:6] == 'libc6_' and x[-4:] == '.deb'
-    libs = [x.text[:-4]
+    is_libc = lambda x: x[:len('libc6_')] == 'libc6_' and x[-len('.deb'):] == '.deb'
+    libs = [x.text[:-len('.deb')]
         for x in [row.find('a') for row in soup.find_all('tr')]
         if x and is_libc(x.text)]
 
@@ -27,10 +28,34 @@ def _search_libc(arch='all'):
     return list(filter(is_arch, libs))
 
 
+def _search_debug(arch='all'):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    is_debug = lambda x: x[:len('libc6-dbg_')] == 'libc6-dbg_' and x[-len('.deb'):] == '.deb'
+    debugs = [x.text[:-len('.deb')]
+        for x in [row.find('a') for row in soup.find_all('tr')]
+        if x and is_debug(x.text)]
+
+    if arch == 'all': return debugs
+
+    is_arch = lambda x: x.split('_')[-1] == arch
+    return list(filter(is_arch, debugs))
+
+
 def list_libc(args):
     arch = args.arch
-    libs = _search_libc(arch)
+    if args.debug:
+        libs = _search_debug(arch)
+    else:
+        libs = _search_libc(arch)
     for lib in libs: print(lib)
+
+
+def list_debug(args):
+    arch = args.arch
+    debugs = _search_debug(arch)
+    for debug in debugs: print(debug)
 
 
 def _extract_libc(file, out):
@@ -52,6 +77,33 @@ def _extract_libc(file, out):
     ar_file.close()
 
 
+def _extract_debug(file, out):
+    ar_file = unix_ar.open(file)
+
+    # can result in key error if data.tar.zst is used
+    tarball = ar_file.open('data.tar.xz')
+    tf = tarfile.open(fileobj=tarball)
+
+    def debug_dir(tar):
+        for t in tar.getmembers():
+            if '/debug/' in t.name:
+                yield t
+
+    tf.extractall(out, debug_dir(tf))
+
+    tf.close()
+    tarball.close()
+    ar_file.close()
+
+
+def _download_file(target, out):
+    with requests.get(target, stream=True) as r:
+        total_size = int(r.headers.get('Content-Length'))
+        with tqdm.wrapattr(r.raw, 'read', total=total_size, desc='') as raw:
+            with open(out, 'wb') as f:
+                shutil.copyfileobj(raw, f)
+
+
 def download_libc(args):
     name = args.target
 
@@ -60,8 +112,20 @@ def download_libc(args):
 
     with tempfile.TemporaryDirectory() as tmp:
         file = os.path.join(tmp, name)
-        urllib.request.urlretrieve(f'{url}{name}.deb', file)
+        _download_file(f'{url}{name}.deb', file)
         _extract_libc(file, out)
+
+
+def download_debug(args):
+    name = args.target
+
+    out = f'{name}'
+    if args.out: out = args.out
+
+    with tempfile.TemporaryDirectory() as tmp:
+        file = os.path.join(tmp, name)
+        _download_file(f'{url}{name}.deb', file)
+        _extract_debug(file, out)
 
 
 def arguments():
@@ -70,12 +134,18 @@ def arguments():
 
     sp_list = subparsers.add_parser('list')
     sp_list.add_argument('-a', '--arch', help='filter by architecture', default='all')
+    sp_list.add_argument('-d', '--debug', help='list debug symbols', action='store_true')
     sp_list.set_defaults(func=list_libc)
 
-    sp_download = subparsers.add_parser('download')
+    sp_download = subparsers.add_parser('get-lib')
     sp_download.add_argument('-t', '--target', help='target libc name', required=True)
     sp_download.add_argument('-o', '--out', help='output file name')
     sp_download.set_defaults(func=download_libc)
+
+    sp_download = subparsers.add_parser('get-dbg')
+    sp_download.add_argument('-t', '--target', help='target libc debug symbols', required=True)
+    sp_download.add_argument('-o', '--out', help='output directory name')
+    sp_download.set_defaults(func=download_debug)
 
     return parser.parse_args()
 
